059738e230643d505e7f8cdf08eb816173db62e8
Abdulics <35041417+Abdulics@users.noreply.github.com>
2021-03-08 17:27:33 -0600

    springboot added

---
+++ passManager/src/main/java/com/dulTek/passManager/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.dulTek.passManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
